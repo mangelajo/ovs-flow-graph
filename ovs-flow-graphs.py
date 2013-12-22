b157b3f549781155986a14c440f176576d642dd0
Miguel Angel Ajo <majopela@redhat.com>
2013-12-22 17:09:50 +0100

    Identify more input sources, simplify actions

--- ovs-flow-graphs.py
+++ ovs-flow-graphs.py
@@ -12,6 +12,8 @@ class OFCTLRulesToDOT:
             else:
                 self.tables[table_number] = [rule]
 
+        self.outputs = {} # outputs will be discovered on the fly
+
     def get_dot(self):
         return self.render()
 
@@ -47,7 +49,7 @@ class OFCTLRulesToDOT:
         self.add(" [")
         self.add("  shape = none")
         self.add('  label = <<table border="0" cellspacing="0">')
-        self.add('   <tr><td port="header" border="1" colspan="5" '
+        self.add('   <tr><td port="header" border="1" colspan="6" '
                  'bgcolor="red">TABLE %d</td></tr>' % table_nr)
         self.add('   <tr>')
         self.add('    <td border="1">in</td>')
@@ -55,6 +57,7 @@ class OFCTLRulesToDOT:
         self.add('    <td border="1">bytes</td>')
         self.add('    <td border="1">idle</td>')
         self.add('    <td border="1">actions</td>')
+        self.add('    <td border="1">outputs</td>')
         self.add('   </tr>')
 
     def add_table_rule(self,rule,rule_id):
@@ -63,24 +66,41 @@ class OFCTLRulesToDOT:
 
         in_port = ""
         if data.has_key('in_port'):
-            in_port +="port:%s" % data['in_port']
+            in_port +="port:%s " % data['in_port']
         if data.has_key('tun_id'):
-            in_port +="tun:%s" % data['tun_id']
+            in_port +="tun:%s " % data['tun_id']
+        if data.has_key('vlan_tci'):
+            in_port +="vlan:%s " % data['vlan_tci']
+        if data.has_key('dl_dst'):
+            in_port +="dst:%s " % data['dl_dst']
+        if data.has_key('dl_vlan'):
+            in_port +="vlan:0x%x " % int(data['dl_vlan'])
 
+
+        outputs_str = ""
         action_str = ""
         for action in actions:
 
             if type(action)==str:
                 action_str += action+","
             if type(action)==dict:
-                action_str += action.keys()[0]+","
+                action_name = action.keys()[0]
+                action_params = action[action_name]
+                if action_name == "output":
+                    outputs_str += action_params +", "
+                elif not action_name in ['resubmit','learn']:
+                    action_str += action_name +", "
+
+
 
         self.add('   <tr>')
         self.add('    <td border="1">%s</td>' % in_port )
         self.add('    <td border="1">%s</td>' % data['n_packets'])
         self.add('    <td border="1">%s</td>' % data['n_bytes'])
         self.add('    <td border="1">%s</td>' % data['idle_age'])
-        self.add('    <td port="rule%d" border="1"> %s </td>'% (rule_id,action_str))
+        self.add('    <td border="1"> %s </td>'% action_str )
+        self.add('    <td port="rule%d" border="1"> %s </td>' %
+                 (rule_id, outputs_str))
         self.add('   </tr>')
 
     def add_table_footer(self,table_nr):
@@ -100,10 +120,22 @@ class OFCTLRulesToDOT:
         actions = rule['actions']
         for action in actions:
             if type(action)==dict:
+                table_rule_str = "table%d:rule%d" %(table_nr,rule_id)
                 for function,params in action.items():
                     if function=="resubmit":
-                        self.add('table%d:rule%d -> table%s:header [label="resubmit"]' %(table_nr,rule_id,params))
-
+                        self.add(table_rule_str +
+                                 ' -> table%s:header [arrowType="vee"]' %
+                                 params)
+                    if function=="learn":
+                        params = params.split(',')
+                        table_param = params[0].split('=')[1]
+                        self.add(table_rule_str +
+                                 ' -> table%s:header [label="learn"]' %
+                                 table_param)
+                    # add connection of outputs to output nodes, it's a mess
+                    #if function=="output":
+                    #    self.add(table_rule_str +
+                    #             ' -> output%s' % params)
 
     def add_footer(self):
         self.add("}\n")
